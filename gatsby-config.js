bcad63b47b4c017995092077c39d1009baf84124
Alex Dobin <adobin@outlook.com>
2018-09-16 15:45:33 -0700

    Fix link in plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
+        icon: `src/assets/headshot.png`,
       },
     },
     `gatsby-plugin-offline`,
