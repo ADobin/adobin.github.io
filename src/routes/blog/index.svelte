b06187e1eaf3760cfd940ca05b43853ff9d88c54
Alex Dobin <adobin@outlook.com>
2021-10-07 22:00:52 -0700

    Fix blog links

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -40,7 +40,7 @@
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
+		<li><a rel="prefetch" href="/blog/{post.slug}">{post.title}</a></li>
 	{/each}
 </ul>
 
