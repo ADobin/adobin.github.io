3ad3cd1f79c390932f85216ae8105fbc563449e8
Alex Dobin <alex@dobin.dev>
2022-03-12 17:14:14 -0800

    Remove unused default metadata function

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,12 +1,6 @@
-import { writable, type get, type Writable } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 export const metadata = writable({
 	title: '',
 	description: ''
 });
-
-type metadataType<T> = T extends Writable<infer U> ? U : never;
-
-export function isDefaultMetadata(data: metadataType<typeof metadata>) {
-	return data.title === '' && data.description === '';
-}
