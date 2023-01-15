979772c5c43001d51c8f6a1e821c1d31921c18fe
Alex Dobin <alex@dobin.dev>
2023-01-15 15:42:16 -0800

    fix(rss): fix item node placement relative to channel in rss

--- src/routes/blog/rss.xml/+server.ts
+++ src/routes/blog/rss.xml/+server.ts
@@ -5,21 +5,18 @@ import type { RequestHandler } from './$types';
 export const prerender = true;
 
 export const GET: RequestHandler = async () => {
-	const rss = create({
-		rss: {
-			'@version': '2.0',
-			channel: {
-				title: "Alex Dobin's blog",
-				link: 'https://alexdobin.com/blog',
-				description: "Alex Dobin's tech and homelab blog"
-			}
-		}
+	const doc = create();
+	const rss = doc.ele('rss', {
+		version: '2.0'
 	});
-
+	const channel = rss.ele('channel');
+	channel.ele('title').txt("Alex Dobin's blog");
+	channel.ele('link').txt('https://alexdobin.com/blog');
+	channel.ele('description').txt("Alex Dobin's tech and homelab blog");
 	const blogPosts = await processBlogIndex(await loadPosts());
 
 	blogPosts.forEach((post) => {
-		const item = rss.first().ele('item');
+		const item = channel.ele('item');
 		item.ele('title').txt(post.metadata.title);
 		item.ele('link').txt(`https://alexdobin.com/blog/${post.metadata.slug}`);
 		item.ele('pubDate').txt(post.metadata.date);
