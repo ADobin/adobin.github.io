dab1ecb0b89ec00cdc325ac5d6684139e63cbbda
Alex Dobin <ADobin@users.noreply.github.com>
2023-06-04 21:55:43 -0700

    feature(babyshower): redirect to baby shower invite (#795)

--- src/routes/babyshower/+page.server.ts
+++ src/routes/babyshower/+page.server.ts
@@ -1,8 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load() {
-	throw redirect(
-		301,
-		'https://mobilizon.alexdobin.com/events/7091304d-56dd-4f8d-9f5f-0c21b8482b6d'
-	);
+	throw redirect(301, 'http://evite.me/XFJJy1hBzS');
 }
